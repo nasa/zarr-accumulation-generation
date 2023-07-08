17247b234c64aab1a3fad59145ae1bfc356f5d09
Hailiang Zhang <hailiang.zhang@nasa.gov>
2023-07-08 00:56:41 -0400

    accumulation_delta_filter detype implemented

--- zarr-accumulation/data_preparation/codec_filter.py
+++ zarr-accumulation/data_preparation/codec_filter.py
@@ -35,13 +35,12 @@ class AccumulationDeltaFilter(Codec):
 
 
     def encode(self, buf):
-        print(buf.shape)
         accumulation_dimension_idx = [index for index, item in enumerate(self.accumulation_dim_order_idx) if item in self.accumulation_dimension]
         accumulation_chunk_shape = list(buf.shape)
         for idx in accumulation_dimension_idx:
             accumulation_chunk_shape[idx] = 1
         accumulation_chunk_shape = tuple(accumulation_chunk_shape)
-        o = np.zeros(accumulation_chunk_shape, dtype='f4')
+        o = np.zeros(accumulation_chunk_shape, dtype=buf.dtype)
         slices = [slice(None)] * len(buf.shape)
         for idx in accumulation_dimension_idx:
             slices[idx] = slice(None, -1)
@@ -56,7 +55,7 @@ class AccumulationDeltaFilter(Codec):
 
     def decode(self, buf, out=None):
         accumulation_dimension_idx = [index for index, item in enumerate(self.accumulation_dim_order_idx) if item in self.accumulation_dimension]
-        enc = np.frombuffer(buf, dtype='f4').reshape(buf.shape)
+        enc = np.frombuffer(buf, dtype=buf.dtype).reshape(buf.shape)
         # IMPORTANT: ONLY 1-D filter suppoted now
         out = np.cumsum(enc, axis=accumulation_dimension_idx[0], out=out)
         return out
