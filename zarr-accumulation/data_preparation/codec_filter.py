a7c0670edb91ef5b2161f0caf096cdf4ebb4c772
Hailiang Zhang <hailiang.zhang@nasa.gov>
2023-07-08 18:19:15 -0400

    accumulation_delta_filter black codes

--- zarr-accumulation/data_preparation/codec_filter.py
+++ zarr-accumulation/data_preparation/codec_filter.py
@@ -4,17 +4,22 @@ import numpy as np
 
 
 class AccumulationDeltaFilter(Codec):
-    codec_id = 'accumulation_delta_filter'
+    codec_id = "accumulation_delta_filter"
 
-    def __init__(self, accumulation_dimension, accumulation_stride, accumulation_dim_order_idx):
+    def __init__(
+        self, accumulation_dimension, accumulation_stride, accumulation_dim_order_idx
+    ):
         self.accumulation_dimension = accumulation_dimension
         self.accumulation_stride = accumulation_stride
         self.accumulation_dim_order_idx = accumulation_dim_order_idx
         return None
 
-
     def encode(self, buf):
-        accumulation_dimension_idx = [index for index, item in enumerate(self.accumulation_dim_order_idx) if item in self.accumulation_dimension]
+        accumulation_dimension_idx = [
+            index
+            for index, item in enumerate(self.accumulation_dim_order_idx)
+            if item in self.accumulation_dimension
+        ]
         accumulation_chunk_shape = list(buf.shape)
         for idx in accumulation_dimension_idx:
             accumulation_chunk_shape[idx] = 1
@@ -25,18 +30,27 @@ class AccumulationDeltaFilter(Codec):
             slices[idx] = slice(None, -1)
         try:
             # IMPORTANT: ONLY 1-D filter suppoted now
-            buf1 = np.concatenate((o, buf[tuple(slices)]), axis=accumulation_dimension_idx[0])
-            out = buf-buf1
+            buf1 = np.concatenate(
+                (o, buf[tuple(slices)]), axis=accumulation_dimension_idx[0]
+            )
+            out = buf - buf1
         except:
-            print(buf.shape, accumulation_chunk_shape, slices, accumulation_dimension_idx)
+            print(
+                buf.shape, accumulation_chunk_shape, slices, accumulation_dimension_idx
+            )
             raise
         return out
 
     def decode(self, buf, out=None):
-        accumulation_dimension_idx = [index for index, item in enumerate(self.accumulation_dim_order_idx) if item in self.accumulation_dimension]
+        accumulation_dimension_idx = [
+            index
+            for index, item in enumerate(self.accumulation_dim_order_idx)
+            if item in self.accumulation_dimension
+        ]
         enc = np.frombuffer(buf, dtype=buf.dtype).reshape(buf.shape)
         # IMPORTANT: ONLY 1-D filter suppoted now
         out = np.cumsum(enc, axis=accumulation_dimension_idx[0], out=out)
         return out
+
 
 numcodecs.register_codec(AccumulationDeltaFilter)
